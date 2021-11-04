37aabd8b885153fad64c68017d480033fac8b0a9
Jan Tokic <jan.tokic@htl-wels.at>
2021-11-04 08:36:32 +0100

    more accurate error messages

--- app/src/main/java/com/willparken/control/RegisterActivity.java
+++ app/src/main/java/com/willparken/control/RegisterActivity.java
@@ -57,7 +57,9 @@ public class RegisterActivity extends AppCompatActivity {
 
     void clickOnRegister(){
         Pattern emailPattern = Pattern.compile("[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\\.[A-Za-z]{2,4}");
+        Pattern phonePattern = Pattern.compile("\\d{7,}");
         Matcher emailMatcher = emailPattern.matcher(txt_email.getText().toString());
+        Matcher phoneMatcher = phonePattern.matcher(txt_tel.getText().toString());
 
         if (!txt_email.getText().toString().equals("") &&
             !txt_password.getText().toString().equals("") &&
@@ -67,29 +69,38 @@ public class RegisterActivity extends AppCompatActivity {
             !txt_address.getText().toString().equals("") &&
             !txt_tel.getText().toString().equals(""))
         {
-            if (emailMatcher.matches() && txt_password.getText().toString().equals(txt_confirmpassword.getText().toString())){
-                iUser = new User(txt_firstname.getText().toString(),
-                        txt_lastname.getText().toString(),
-                        null,
-                        txt_address.getText().toString(),
-                        txt_tel.getText().toString(),
-                        txt_email.getText().toString(),
-                        User.encryptPassword(txt_password.getText().toString())
-                );
-                if (SerializationFactory.getInstance().exists(iUser)){
+            if(!emailMatcher.matches()){
+                Toast.makeText(getApplicationContext(),"Email is not valid!",Toast.LENGTH_LONG).show();
+                return;
+            }
+            if(!txt_password.getText().toString().equals(txt_confirmpassword.getText().toString())){
+                Toast.makeText(getApplicationContext(),"Passwords do not match!",Toast.LENGTH_LONG).show();
+                return;
+            }
+            if(!phoneMatcher.matches()){
+                Toast.makeText(getApplicationContext(),"Phone number is not valid!",Toast.LENGTH_LONG).show();
+                return;
+            }
+            iUser = new User(txt_firstname.getText().toString(),
+                    txt_lastname.getText().toString(),
+                    null,
+                    txt_address.getText().toString(),
+                    txt_tel.getText().toString(),
+                    txt_email.getText().toString(),
+                    User.encryptPassword(txt_password.getText().toString())
+
+            );
+            if (SerializationFactory.getInstance().exists(iUser)){
                     Toast.makeText(getApplicationContext(),"Email already in use!",Toast.LENGTH_LONG).show();
                     return;
-                }
-                iUser.save();
-                SerializationFactory.getInstance().persist(getApplicationContext());
-
-                Toast.makeText(getApplicationContext(),"Account created successfully!",Toast.LENGTH_LONG).show();
-                finish();
-                intentHome.putExtra("iUser",iUser);
-                startActivity(intentHome);
-            }else{
-                Toast.makeText(getApplicationContext(),"Please check your input.",Toast.LENGTH_LONG).show();
             }
+            iUser.save();
+            SerializationFactory.getInstance().persist(getApplicationContext());
+
+            Toast.makeText(getApplicationContext(),"Account created successfully!",Toast.LENGTH_LONG).show();
+            finish();
+            intentHome.putExtra("iUser",iUser);
+            startActivity(intentHome);
         }else{
             Toast.makeText(getApplicationContext(),"Please fill in all Fields.",Toast.LENGTH_LONG).show();
         }
