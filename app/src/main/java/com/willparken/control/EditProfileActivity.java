e9ec8c4bc36e13ea38cc4cf04869d0639d139e65
saidov-arb <arbi.saidov@htl-wels.at>
2022-04-28 10:19:40 +0200

    Minimal Extension of ViewProfile and EditProfile, Controller and XML

--- app/src/main/java/com/willparken/control/EditProfileActivity.java
+++ app/src/main/java/com/willparken/control/EditProfileActivity.java
@@ -1,9 +1,10 @@
 package com.willparken.control;
 
+import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -11,37 +12,42 @@ import com.willparken.R;
 
 public class EditProfileActivity extends AppCompatActivity {
 
-    private Button cancelButton;
-    private Button saveButton;
+    private Button editprofile_btn_cancel;
+    private Button editprofile_btn_save;
+    private TextView editprofile_txt_firstname, editprofile_txt_lastname, editprofile_txt_tel, editprofile_txt_password;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_editprofile);
 
-        cancelButton = (Button) findViewById(R.id.editprofile_cancel);
-        saveButton = (Button) findViewById(R.id.editprofile_save);
-        cancelButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
+        editprofile_btn_cancel = (Button) findViewById(R.id.editprofile_btn_cancel);
+        editprofile_btn_save = (Button) findViewById(R.id.editprofile_btn_save);
 
-                openView();
-            }
-        });
+        editprofile_txt_firstname = (TextView) findViewById(R.id.editprofile_txt_firstname);
+        editprofile_txt_lastname = (TextView) findViewById(R.id.editprofile_txt_lastname);
+        editprofile_txt_tel = (TextView) findViewById(R.id.editprofile_txt_tel);
+        editprofile_txt_password = (TextView) findViewById(R.id.editprofile_txt_password);
 
-        saveButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
+        editprofile_btn_cancel.setOnClickListener(e -> clickOnCancel());
+        editprofile_btn_save.setOnClickListener(e -> clickOnCancel());
 
-                openView();
-            }
-        });
+        fillFields();
     }
 
-    public void openView(){
+    public void clickOnCancel(){
 
         Intent intent = new Intent(this, ViewProfileActivity.class);
         startActivity(intent);
+        finish();
+    }
+
+    @SuppressLint("SetTextI18n")
+    private void fillFields(){
+        editprofile_txt_firstname.setText(DashboardActivity.currentUser.getFirstname());
+        editprofile_txt_lastname.setText(DashboardActivity.currentUser.getLastname());
+        editprofile_txt_tel.setText(DashboardActivity.currentUser.getTelephoneNumber());
+        editprofile_txt_password.setText("");
     }
 }
 
--- app/src/main/java/com/willparken/control/ViewProfileActivity.java
+++ app/src/main/java/com/willparken/control/ViewProfileActivity.java
@@ -2,36 +2,48 @@ package com.willparken.control;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 
 import com.willparken.R;
 
 
 public class ViewProfileActivity extends AppCompatActivity {
 
-    private Button editButton;
+    private Button viewprofile_btn_edit;
+    private TextView viewprofile_txt_firstname, viewprofile_txt_lastname, viewprofile_txt_tel, viewprofile_txt_password;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_viewprofile);
 
-        editButton = (Button) findViewById(R.id.viewprofile_edit);
-        editButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
+        viewprofile_btn_edit = (Button) findViewById(R.id.viewprofile_btn_edit);
+        viewprofile_txt_firstname = (TextView) findViewById(R.id.viewprofile_txt_firstname);
+        viewprofile_txt_lastname = (TextView) findViewById(R.id.viewprofile_txt_lastname);
+        viewprofile_txt_tel = (TextView) findViewById(R.id.viewprofile_txt_tel);
+        viewprofile_txt_password = (TextView) findViewById(R.id.viewprofile_txt_password);
 
-                openEdit();
-            }
-        });
+        viewprofile_btn_edit.setOnClickListener(e -> clickOnEdit());
+
+        fillFields();
     }
 
-    public void openEdit(){
+    private void clickOnEdit(){
 
         Intent intent = new Intent(this, EditProfileActivity.class);
         startActivity(intent);
+        finish();
+    }
+
+    @SuppressLint("SetTextI18n")
+    private void fillFields(){
+        viewprofile_txt_firstname.setText(viewprofile_txt_firstname.getText()+" "+DashboardActivity.currentUser.getFirstname());
+        viewprofile_txt_lastname.setText(viewprofile_txt_lastname.getText()+" "+DashboardActivity.currentUser.getLastname());
+        viewprofile_txt_tel.setText(viewprofile_txt_tel.getText()+" "+DashboardActivity.currentUser.getTelephoneNumber());
+        viewprofile_txt_password.setText(viewprofile_txt_password.getText()+" "+"*******");
     }
 }
